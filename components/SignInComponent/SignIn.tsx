02f14cb949871982997c9f54178aa849c95db966
jhon botero <jhonbg7@gmail.com>
2023-09-26 17:10:59 -0500

    Se resuelve conflicto de paddings

--- components/SignInComponent/SignIn.tsx
+++ components/SignInComponent/SignIn.tsx
@@ -34,8 +34,9 @@ const SignIn = () => {
             border: 'solid',
             borderColor: 'white',
             borderRadius: 5,
-            paddingX: 6,
-            paddingY: 2, 
+            paddingX: 5,
+            paddingY: 7,
+            boxShadow: 10, 
           }}
         >
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
