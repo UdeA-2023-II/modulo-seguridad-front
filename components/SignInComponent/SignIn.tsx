a649f57770e218fdd9017a441bd3065168848a89
yeison8a <yeison.ochoa@udea.edu.co>
2023-10-11 16:15:12 -0500

    conexión con endpoint de prueba

--- components/SignInComponent/SignIn.tsx
+++ components/SignInComponent/SignIn.tsx
@@ -1,22 +1,52 @@
 import * as React from 'react';
+import { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Typography } from '@mui/material';
 
-
-// TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
 const SignIn = () => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const form = event.target as HTMLFormElement;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    try {
+      const response = await fetch('https://6513538b8e505cebc2e9c6bc.mockapi.io/api/v1/users', {
+        method: 'GET', 
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const users = await response.json();
+
+        const isValidCredentials = users.some((user) => user.mail === email && user.password === password);
+
+        if (isValidCredentials) {
+          console.log('Inicio de sesión exitoso');
+        } else {
+          console.error('Credenciales incorrectas');
+        }
+      } else {
+        console.error('Error al obtener datos de la API');
+      }
+    } catch (error) {
+      console.error('Error de red:', error);
+    }
   };
 
   return (
@@ -37,27 +67,18 @@ const SignIn = () => {
             boxShadow: 10,
           }}
         >
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }} className="block text-center mx-auto">
-            <Typography
-              variant='h3'
-              component='h3'
-              fontSize='20px'
-              textAlign='center'>¿Cual es tu usuario o correo electrónico?</Typography>
+          <form onSubmit={handleSubmit} noValidate sx={{ mt: 1 }} className="block text-center mx-auto">
+            <span className="block text-center mx-auto">Usuario</span>
             <TextField
               margin="normal"
               required
               fullWidth
               id="email"
-              label="Ingresa tu usuario o email"
+              label="¿Cuál es tu usuario o correo electrónico?"
               name="email"
               autoComplete="email"
             />
-            <Typography
-              variant='h3'
-              component='h3'
-              fontSize='20px'
-              textAlign='center'
-              paddingTop='20px'>Contraseña</Typography>
+            <span className='text-center'>Contraseña</span>
             <TextField
               margin="normal"
               required
@@ -77,18 +98,17 @@ const SignIn = () => {
             >
               INGRESAR
             </Button>
-            <Grid container>
-              <Grid item xs={12} textAlign='center'>
-                <Link href="/" variant="body2">
-                  {"¿Olvidaste tu contraseña?"}
-                </Link>
-              </Grid>
+          </form>
+          <Grid container>
+            <Grid item xs={12}>
+              <Link href="/" variant="body2">
+                {"¿Olvidaste tu contraseña?"}
+              </Link>
             </Grid>
-          </Box>
+          </Grid>
         </Box>
       </Container>
     </ThemeProvider>
-
   );
 }
 
