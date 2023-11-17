e6be05452290c3dac2bacbc82952c053e037681a
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2023-11-17 10:20:48 -0500

    Se agrega imágen de fondo para el dashboar

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,15 +1,22 @@
-import { SidebarComponent } from '../components/sidebar/SidebarComponent'
+import { SidebarComponent } from '@/components/sidebar/SidebarComponent';
+import React from 'react';
 
-const Dashboard = () => {
+// eslint-disable-next-line react/function-component-definition
+export default function Dashboard() {
+    const backgroundImage = '/images/imagen-landing.jpg';
+
     return (
-        <div>
-
-            <main className=''>
-                < SidebarComponent />
+        <div className="flex">
+            <SidebarComponent />
+            <main
+                className="w-3/4 bg-cover bg-no-repeat"
+                style={{
+                    backgroundImage: `url("${backgroundImage}")`,
+                    minHeight: '100vh',
+                    maxHeight: '100vh',
+                }}
+            >
             </main>
         </div>
-
     );
-};
-
-export default Dashboard;
+}
